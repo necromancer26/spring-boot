b8c5b3b824a29625fdc7b32ecd7e145990eeb3bb
Khalil <khalil.chettaoui06@gmail.com>
2023-02-07 12:44:36 +0100

    setting up project

--- src/main/java/com/spring/skistation/SkiStationApplication.java
+++ src/main/java/com/spring/skistation/SkiStationApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SkiStationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SkiStationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SkiStationApplication.class, args);
+        System.out.println("hello world");
+    }
 
 }
